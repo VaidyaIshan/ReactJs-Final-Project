449e852fc53062204c87b7140c6c3aa822b43a06
IshanBaidya <165746811+VaidyaIshan@users.noreply.github.com>
2025-08-23 07:48:37 +0545

    Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    
+    <div className='items-center flex justify-center text-9xl bg-red-400'>
+      <p>
+        Ishan
+      </p>
+      
+    </div>
+     
+    
+  )
+}
+
+export default App
